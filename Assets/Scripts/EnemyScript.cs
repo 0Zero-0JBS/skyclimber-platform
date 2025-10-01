f41eda6ba3e83672f513fcc003f261d9cbf5d002
0Zero-0JBS <550264@escg.ac.uk>
2025-10-01 15:46:39 +0100

    Fixed enemy script, added helper script, added death animation to player

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,15 +5,23 @@ public class EnemyScript : MonoBehaviour
 {
     //Set this to the computer
     public PlayerMove playerMove;
+    HelperScript helper;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Add the helper script and store a reference to it                                               
+        helper = gameObject.AddComponent<HelperScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
         print("Enemy says: The player has " + playerMove.lives + "lives ");
+        if (Input.GetKey("space"))
+        {
+            helper.DoFlipObject(true);    // this will execute the method in HelperScript.cs
+        }
+
     }
 }
